123dc4157804b0c47dfd2f4a1cb687efb5b0fccc
Maëlle Lagarde <maelle.lagarde@MacBook-Pro-de-Maelle.local>
2023-05-09 11:34:06 +0200

    vf

---
+++ jour01/job02/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hello LaPlateforme!\n"; /* ceci est un commentaire */
+echo "Hello LaPlateforme!\n"; # ceci est aussi un commentaire.
+?>
+
+</body>
+</html>
